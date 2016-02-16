ecea02b65a14ac86df7c841dcd4597aa8cdbecce
softwarespot <softwarespotapps@gmail.com>
2016-02-16 20:32:25 +0200

    Minor PSR-2 fixes

--- createGUID.php
+++ createGUID.php
@@ -12,7 +12,8 @@ function createGUID()
         return com_create_guid();
     }
 
-    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X',
+    return sprintf(
+        '%04X%04X-%04X-%04X-%04X-%04X%04X%04X',
         mt_rand(0, 65535),
         mt_rand(0, 65535),
         mt_rand(0, 65535),
@@ -20,5 +21,6 @@ function createGUID()
         mt_rand(32768, 49151),
         mt_rand(0, 65535),
         mt_rand(0, 65535),
-        mt_rand(0, 65535));
+        mt_rand(0, 65535)
+    );
 }
