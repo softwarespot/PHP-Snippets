a2a95fd6b1a97558629d6de286b68a9a7beea231
softwarespot <softwarespotapps@gmail.com>
2016-02-16 21:51:13 +0200

    Added array_get

---
+++ array_get.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Get a value from an array based on a particular key
+ *
+ * @access public
+ * @param mixed $needle Key to search for
+ * @param array $haystack Array to search in
+ * @return mixed|null The value from the array; otherwise, null
+ */
+function array_get($needle, &$haystack)
+{
+    return array_key_exists($needle, $haystack) ? $haystack[$needle] : null;
+}
