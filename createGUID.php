3277f61ace1b8f4ac8c4e435ce14a8cb27bc5b50
softwarespot <softwarespotapps@gmail.com>
2016-02-16 19:49:33 +0200

    Added createGUID

---
+++ createGUID.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Create a GUID
+ *
+ * @access public
+ * @return string Random GUID
+ */
+function createGUID()
+{
+    if (function_exists('com_create_guid')) {
+        return com_create_guid();
+    }
+
+    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X',
+        mt_rand(0, 65535),
+        mt_rand(0, 65535),
+        mt_rand(0, 65535),
+        mt_rand(16384, 20479),
+        mt_rand(32768, 49151),
+        mt_rand(0, 65535),
+        mt_rand(0, 65535),
+        mt_rand(0, 65535));
+}
