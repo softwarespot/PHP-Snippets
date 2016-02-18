4681d473f4ed778aea8eb4cd587130b1a0a72a43
softwarespot <softwarespotapps@gmail.com>
2016-02-18 20:28:45 +0200

    Added isValidIP

---
+++ isValidIP.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Validate an IP address
+ * Idea by CodeIgniter, URL: https://github.com/bcit-ci/CodeIgniter/blob/master/system/core/Input.php
+ *
+ * @param string $ip IP address to validated
+ * @param string $type IP protocol: 'ipv4' or 'ipv6'
+ * @return bool True, is a valid IP address; otherwise, false
+ */
+function isValidIP($ip, $type = null)
+{
+    switch (strtolower($type)) {
+        case 'ipv4':
+            $type = FILTER_FLAG_IPV4;
+            break;
+
+        case 'ipv6':
+            $type = FILTER_FLAG_IPV6;
+            break;
+
+        default:
+            $type = null;
+            break;
+    }
+
+    return (bool) filter_var($ip, FILTER_VALIDATE_IP, $type);
+}
