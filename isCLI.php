bc46d28ccf0d44cb0796b93b9588cdd2cd58747c
softwarespot <softwarespotapps@gmail.com>
2016-02-18 22:06:23 +0200

    Added isCLI

---
+++ isCLI.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Is the command-line interface (CLI)
+ * Idea by PHP, URL: http://php.net/manual/en/features.commandline.php
+ *
+ * @return boolean True, using the CLI; otherwise, false
+ */
+function isCLI()
+{
+    return (PHP_SAPI === 'cli' || defined('STDIN'));
+}
