d48b39f95567069303cc384a765965d48e4db6fe
softwarespot <softwarespotapps@gmail.com>
2016-03-15 20:54:45 +0200

    Minor fixes

--- isIP.php
+++ isIP.php
@@ -6,14 +6,20 @@
  *
  * @access public
  * @param string $ip IP address to validated
- * @param string $type IP protocol: 'ipv4' or 'ipv6'
+ * @param string $type IP protocol: 'ipv4' or 'ipv6'. Default is 'ipv4'
+ * @param boolean $exludePrivAndRes Exclude private and reserved ranges. Default it false
  * @return boolean True, is a valid IP address; otherwise, false
  */
-function isIP($ip, $type = null)
+function isIP($ip, $type = FILTER_FLAG_IPV4, $exludePrivAndRes = false)
 {
-    $ip = strtolower($type);
+    // Check if the value is falsy
+    if (empty($ip)) {
+        return false;
+    }
 
-    switch ($ip) {
+    $type = strtolower($type);
+
+    switch ($type) {
         case 'ipv4':
             $type = FILTER_FLAG_IPV4;
             break;
@@ -23,8 +29,17 @@ function isIP($ip, $type = null)
             break;
 
         default:
-            $type = null;
+            $type = FILTER_FLAG_IPV4;
             break;
+    }
+
+    // Check the default value is boolean
+    is_bool($exludePrivAndRes) || $exludePrivAndRes = false;
+
+    if ($exludePrivAndRes) {
+        // Use bitwise OR when excluding the private and reserved address ranges
+        $type |= FILTER_FLAG_NO_PRIV_RANGE;
+        $type |= FILTER_FLAG_NO_RES_RANGE;
     }
 
     return (bool) filter_var($ip, FILTER_VALIDATE_IP, $type);
--- stringCompact.php
+++ stringCompact.php
@@ -4,15 +4,16 @@
  * Compact a string to a maximum length
  *
  * @access public
- * @param string $value String to compact
+ * @param string $str String to compact
  * @param integer $length Length to trim at
  * @return string Compact string; otherwise, original string
  */
-function stringCompact($value, $length = 0)
+function stringCompact($str, $length = 0)
 {
-    if ($length === 0 || mb_strlen($value) <= $length) {
-        return $value;
+    // mb_strwidth is better than using mb_strlen. See PHP docs for more details
+    if ($length === 0 || mb_strwidth($str) <= $length) {
+        return $str;
     }
 
-    return mb_strimwidth($value, 0, $length, '...');
+    return mb_strimwidth($str, 0, $length, '...');
 }
